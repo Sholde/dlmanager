846a8b552cff31a31820e2b5f919edbeb79a1d87
Sholde <exshode@gmail.com>
2021-10-30 15:30:00 +0200

    fix move script

--- move.sh
+++ move.sh
@@ -2,66 +2,49 @@
 
 # Move pdf file to pdf folder
 move_pdf() {
-  pdf_files=${dl_path}*.pdf
-  if [ -f ${pdf_files} ] ; then
+  pdf_files=$(ls ${dl_path}*.pdf 2> /dev/null)
+  if [ "" != "${pdf_files}" ] ; then
     mv ${pdf_files} ${pdf_folder_path}
   fi
 }
 
 # Move image file to img folder
 move_img() {
-  png_files=${dl_path}*.png
-  if [ -f ${png_files} ] ; then
-    mv ${png_files} ${img_folder_path}
-  fi
-
-  jpeg_files=${dl_path}*.jpeg
-  if [ -f ${jpeg_files} ] ; then
-    mv ${jpeg_files} ${img_folder_path}
+  img_files=$(ls ${dl_path}*.{png,jpeg,jpg} 2> /dev/null)
+  if [ "" != "${img_files}" ] ; then
+    mv ${img_files} ${img_folder_path}
   fi
 }
 
 # Move archive file to archive folder
 move_archive() {
-  zip_files=${dl_path}*.zip
-  if [ -f ${zip_files} ] ; then
-    mv ${zip_files} ${archive_folder_path}
-  fi
-
-  tar_files=${dl_path}*.tar
-  if [ -f ${tar_files} ] ; then
-    mv ${tar_files} ${archive_folder_path}
-  fi
-
-  tar_gz_files=${dl_path}*.tar.gz
-  if [ -f ${tar_gz_files} ] ; then
-    mv ${tar_gz_files} ${archive_folder_path}
+  archive_files=$(ls ${dl_path}*.{zip,tar,tar.gz} 2> /dev/null)
+  if [ "" != "${archive_files}" ] ; then
+    mv ${archive_files} ${archive_folder_path}
   fi
 }
 
 # Move all code files to code folder
 move_code() {
-  c_files=${dl_path}*.c
-  if [ -f ${c_files} ] ; then
-    mv ${c_files} ${code_folder_path}
-  fi
-
-  h_files=${dl_path}*.h
-  if [ -f ${h_files} ] ; then
-    mv ${h_files} ${code_folder_path}
+  code_files=$(ls ${dl_path}*.{c,cpp,cc,h,hpp,hh} 2> /dev/null)
+  if [ "" != "${code_files}" ] ; then
+    mv ${code_files} ${code_folder_path}
   fi
 }
 
 # Move all code files to code folder
 move_text() {
-  txt_files=${dl_path}*.txt
-  if [ -f ${txt_files} ] ; then
-    mv ${txt_files} ${text_folder_path}
+  text_files=$(ls ${dl_path}*.{txt,ods,md,org,dat,csv,data} 2> /dev/null)
+  if [ "" != "${text_files}" ] ; then
+    mv ${text_files} ${text_folder_path}
   fi
+}
 
-  ods_files=${dl_path}*.ods
-  if [ -f ${ods_files} ] ; then
-    mv ${ods_files} ${text_folder_path}
+# Move all code files to code folder
+move_text() {
+  text_files=$(ls ${dl_path}*.{txt,ods,md,org,dat,csv,data} 2> /dev/null)
+  if [ "" != "${text_files}" ] ; then
+    mv ${text_files} ${text_folder_path}
   fi
 }
 
